3666594e2d91b813149f2be7f9769efc5bde5ebd
Bing Liu <bing.liu6@merck.com>
2024-06-20 00:57:12 -0400

    D

--- Comparison/main - 1 scenario/main - cities simulator - doparallel.R
+++ Comparison/main - 1 scenario/main - cities simulator - doparallel.R
@@ -49,7 +49,7 @@ source("Comparison/utility functions/f_doparallel_1sim_3m.r")
 
 # [1.b] Parameters in cities simulator [nSim = 300]
 {
-  nSim  = 14                                                                        # number of simulated trials
+  nSim  = 300                                                                    # number of simulated trials
   n_patient_ctrl = 200
   n_patient_expt = 200
   
